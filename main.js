25bcc40b38e997d50576cda790a4d9c0af6c36ee
rebeccadev <rebecca.redfern@hotmail.co.uk>
2020-11-23 13:51:11 +0000

    Add function to JS

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var xhr = new XMLHttpRequest();
+  
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
 
-xhr.send();
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
